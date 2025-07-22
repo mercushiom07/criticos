c848d1d7d3dbe846c03a8aa6dbf104750f15425c
Uribe <isaias.uribe@aptiv.com>
2025-07-22 09:34:17 -0600

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/criticos/sw.js', { scope: '/criticos/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
